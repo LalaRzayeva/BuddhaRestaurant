e07ecf0c14b2c018b311859725c15998af74d797
Lala Rzayeva <lali.rzayeva@gmail.com>
2017-07-23 14:39:57 +0200

    Initial commit

---
+++ javascript/scroll_animate.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.page-header', {
+  duration: 2000,
+  origin: 'bottom'
+});
+
+sr.reveal('.col-lg-12', {
+  duration: 2000,
+  origin: 'bottom'
+});
+
+sr.reveal('.col-lg-4', {
+  duration: 2000,
+  origin: 'bottom'
+});
+
+sr.reveal('#myCarousel', {
+  duration: 2000,
+  origin: 'bottom'
+});
